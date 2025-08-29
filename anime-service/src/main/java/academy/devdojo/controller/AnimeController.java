a41693d4ae1cabd17d9df5a7ce4bd0b0f7cbe375
Wesley Bruno <wesleybruno910@gmail.com>
2025-08-29 11:45:08 -0300

    video12: Exercise

--- anime-service/src/main/java/academy/devdojo/controller/AnimeController.java
+++ anime-service/src/main/java/academy/devdojo/controller/AnimeController.java
@@ -1,22 +1,34 @@
 package academy.devdojo.controller;
 
+import academy.devdojo.domain.Anime;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.concurrent.TimeUnit;
+import java.util.Objects;
 
 @Slf4j
 @RestController
 @RequestMapping("v1/animes")
 public class AnimeController {
 
-    @GetMapping
-    public List<String> listAll() throws InterruptedException {
-        log.info(Thread.currentThread().getName());
-        TimeUnit.SECONDS.sleep(1);
-        return List.of("Narurto", "DBZ", "Demon Slayer");
+    @GetMapping()
+    public List<Anime> listAllParam(@RequestParam(required = false) String name) {
+        if (Objects.isNull(name))
+            return Anime.hardCoded();
+
+        return Anime.hardCoded()
+                .stream()
+                .filter(anime -> anime.getName().equalsIgnoreCase(name))
+                .toList();
+    }
+
+    @GetMapping("{id}")
+    public Anime findById(@PathVariable Long id) {
+        return Anime.hardCoded()
+                .stream()
+                .filter(anime -> anime.getId().equals(id))
+                .findFirst()
+                .orElse(null);
     }
 }
---
+++ anime-service/src/main/java/academy/devdojo/domain/Anime.java
@@ -0,0 +1,48 @@
+package academy.devdojo.domain;
+
+import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Builder
+public class Anime {
+
+    private Long id;
+    private String name;
+
+    public static List<Anime> hardCoded() {
+        List<Anime> list = new ArrayList<>();
+
+        list.add(Anime.builder()
+                .id(1L)
+                .name("Naruto")
+                .build());
+
+        list.add(Anime.builder()
+                .id(2L)
+                .name("One Piece")
+                .build());
+
+        list.add(Anime.builder()
+                .id(3L)
+                .name("Sakura Quest")
+                .build());
+
+        list.add(Anime.builder()
+                .id(4L)
+                .name("Full Metal Alchemist Brotherhood")
+                .build());
+
+        list.add(Anime.builder()
+                .id(5L)
+                .name("Digimon")
+                .build());
+
+        return list;
+    }
+}
